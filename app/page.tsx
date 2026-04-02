d98eba925db215031920aef6d2fe49dffa1a98b1
agustinsosa10 <agustin.sos.m10@gmail.com>
2026-04-02 17:18:44 -0300

    feat: proyectos carousel with 1+2 card layout and nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
+import ProyectosCarousel from "@/components/ProyectosCarousel";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <ProyectosCarousel />
     </>
   );
 }
